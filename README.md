80f4a34b50f3f78c9db507c82344c55ed318e86f
mstva <mustafa.abdulluh@gmail.com>
2022-10-28 15:21:30 +0200

    added production environment

--- README.md
+++ README.md
@@ -58,3 +58,24 @@ A backend API using Python, Django and PostgresSQL.
   ssh root@server_api_address "docker logs crewtech_staging_django"
   ```
 
+#### Production Environment:
+- Set Environment Variables:
+  - Copy `crewtech_api/.env.sample/` folder and rename it to `crewtech_api/.env/`.
+  - Update the `crewtech_api/.env/.env.production` file.
+- Run the script to start docker compose:
+  ```shell
+  sh crewtech_scripts/run_production.sh local
+  ```
+- Push to the server:
+  ```shell
+  rsync -r crewtech_api crewtech_scripts/run_production.sh root@server_api_address:/root/crewtech
+  ```
+- Run the script on the server:
+  ```shell
+  ssh root@server_api_address "sh crewtech/run_production.sh"
+  ```
+- Check logs:
+  ```shell
+  ssh root@server_api_address "docker logs crewtech_production_django"
+  ```
+
---
+++ crewtech_api/src/settings/production.py
@@ -0,0 +1,28 @@
+from .base import *
+import os
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+AWS_ACCESS_KEY_ID = os.environ.get('SPACE_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('SPACE_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('SPACE_NAME')
+AWS_S3_ENDPOINT_URL = os.environ.get('SPACE_ENDPOINT_URL')
+AWS_S3_LOCATION_URL = os.environ.get('SPACE_LOCATION_URL')
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+STATIC_URL = f'{AWS_S3_LOCATION_URL}/static/'
+STATICFILES_STORAGE = 'src.storage.StaticStorage'
+
+MEDIA_URL = f'{AWS_S3_LOCATION_URL}/media/'
+DEFAULT_FILE_STORAGE = 'src.storage.MediaStorage'
