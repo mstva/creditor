93a87346bf32f14c7657a37abe43a425bee539b7
mstva <mustafa.abdulluh@gmail.com>
2022-11-03 16:17:43 +0200

    refactor compose file path

--- crewtech_scripts/get_infra_output.py
+++ crewtech_scripts/get_infra_output.py
@@ -7,7 +7,7 @@ parser.add_argument("--env", help="Current Environment")
 args = parser.parse_args()
 env = args.env
 
-compose = "../crewtech_infrastructure/docker-compose.yml"
+compose = "crewtech_infrastructure/docker-compose.yml"
 run_command = f'docker compose -f {compose} run --rm terraform -chdir=src output -json {env}'
 
 process = subprocess.run(run_command, shell=True, stdout=subprocess.PIPE)
