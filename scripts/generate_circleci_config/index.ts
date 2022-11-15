e22a10be8b57309eb45bf6cf6b30a453af52933a
mstva <mustafa.abdulluh@gmail.com>
2022-11-15 13:37:51 +0200

    remove pytest and added ls -a

--- scripts/generate_circleci_config/index.ts
+++ scripts/generate_circleci_config/index.ts
@@ -28,7 +28,7 @@ const run_unit_tests_job = () => {
     job.addStep(new CircleCI.commands.Run({
         name: "Run Pytest",
         working_directory: "backend",
-        command: `pytest`
+        command: `ls -a`
     }))
     return job
 }
